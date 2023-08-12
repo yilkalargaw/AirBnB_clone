8787911a07f1c28a599f6caf376e909f158caea4
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-08-12 15:27:22 +0000

    add create method for console.py's HBNBCommand class

--- console.py
+++ console.py
@@ -10,17 +10,26 @@ class HBNBCommand(cmd.Cmd):
     Class for Interperator
     """
 
+    prompt = "(hbnb) "
+
+    __hbnb_class_map = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review,
+    }
+    __hbnb_methods = ['create', 'show', 'destroy', 'all', 'update', 'count']
+
+    error_occured = False
+
     def do_quit(self, arg):
         """
         Quits the command Loop.
         """
         exit()
-
-    def do_EOF(self, arg):
-        """
-        End-of-file
-        """
-        return True
 
     def help_quit(self):
         """
@@ -28,11 +37,38 @@ class HBNBCommand(cmd.Cmd):
         """
         print("Quit command to exit the program")
 
+    def do_EOF(self, arg):
+        """
+        End-of-file
+        """
+        return True
+
     def help_EOF(self):
         """
         Help for EOF
         """
         print("End of File")
+
+    def do_create(self, arg):
+        """ Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id. Ex: $ create BaseModel
+           - If the class name is missing, prints ** class name missing **
+           - If the class name doesn’t exist, prints ** class doesn't exist **
+        """
+
+        if !(arg):
+            print('** class name missing **')
+        elif arg not in self.__hbnb_class_map:
+            print('** class doesn\'t exist **')
+        else:
+            arg=self.__hbnb_class_map(arg)
+            arg.save()
+            print(arg.id)
+
+    def help_create(self):
+        """Help for create"""
+        print('Creates an instance of a Base Model, saves it to  a JSON file')
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
