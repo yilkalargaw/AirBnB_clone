12d7a2aaab6fe1e759063eec9df49d458fa6072d
Youssef <youssefkaramedu@gmail.com>
2023-08-13 03:35:26 -0700

    Change code

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,22 @@
-import datetime
 import uuid
-
+from datetime import datetime
 
 class BaseModel:
-
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        # Add other instance attributes specific to your subclasses here
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        dic = self.__dict__.copy()
-        dic[__class__] = self.__class__.__name__
-        dic['created_at'] = self.created_at.isoformat()
-        dic['updated_at'] = self.updated_at.isoformat()
-        return dic
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
---
+++ models/base_model.py.old
@@ -0,0 +1,24 @@
+import datetime
+import uuid
+
+
+class BaseModel:
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        dic = self.__dict__.copy()
+        dic[__class__] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
