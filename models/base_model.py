b97063f0a20cbeeff9e6322e4acc247977d90607
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-08-13 13:02:14 +0200

    Add docstring

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 import uuid
+"""Module for Base class""" 
 
 
 class BaseModel:
+"""Base class"""
 
     def __init__(self):
         self.id = str(uuid.uuid4())
@@ -10,13 +12,16 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """Returns a human-readable string representation"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """Updates current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary representation of an instance."""
         dic = self.__dict__.copy()
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
