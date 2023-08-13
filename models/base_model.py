10df972591e21835f3c74b44e6f5a92bb748a131
Youssef <youssefkaramedu@gmail.com>
2023-08-13 03:52:08 -0700

    Remove datetime in func

--- models/base_model.py
+++ models/base_model.py
@@ -6,19 +6,19 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dic = self.__dict__.copy()
-        dic[__class__] = self.__class__.__name__
+        dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
         dic['updated_at'] = self.updated_at.isoformat()
         return dic
