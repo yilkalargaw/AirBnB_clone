5bd5bfb633a46849d680e3df4813cfdb5e64ba50
Youssef <youssefkaramedu@gmail.com>
2023-08-13 03:41:22 -0700

    Add old implementation

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,24 @@
-import uuid
 from datetime import datetime
+import uuid
+
 
 class BaseModel:
+
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        # Add other instance attributes specific to your subclasses here
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        dic = self.__dict__.copy()
+        dic[__class__] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
