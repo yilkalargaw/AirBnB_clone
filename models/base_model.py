87d12a99ef214d95cb374f5022da90e30e4474a0
Youssef <youssefkaramedu@gmail.com>
2023-08-13 14:38:44 -0700

    Add Basemodel from dic

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,19 @@ from datetime import datetime
 class BaseModel:
     """Base class that will be inherited later"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs not None:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                date_format = "%Y-%m-%dT%H:%M:%S.%f"
+                if key == 'created_at' or key == 'updated at':
+                    value = datetime.strptime(value, date_format)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a human-readable string representation"""
